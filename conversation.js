fa2bbf96a05dd4e280c7c5746811d760bde6772e
Nicolas Binand <nicolas.binand@hoppen.care>
2025-01-20 14:09:02 +0100

    Mock with callback

--- conversation.js
+++ conversation.js
@@ -2,14 +2,23 @@ exec = require('cordova/exec');
 var conversations = {
   open: function(callTo,token,succ,fail) {
 	//mock
+	if (succ) {
+		return succ(true);
+	}
 	return true;
   },
   checkPermissions: function(succ,fail) {
 	//mock
+	if (succ) {
+		return succ(true);
+	}
 	return true;
   },
   requestPermissions: function(succ,fail) {
 	//mock
+	if (succ) {
+		return succ(true);
+	}
 	return true;
   },
   isDummyPlugin: true
